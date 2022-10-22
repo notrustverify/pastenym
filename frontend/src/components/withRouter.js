4c773d55c2f2d41a2991032de9810294ad627aa9
sven <s@ono.re>
2022-10-22 13:08:17 +0200

    add: detect protocol and errormodal

---
+++ frontend/src/components/withRouter.js
@@ -0,0 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+import React from 'react';
+
+export const withRouter = (Component) => {
+  const Wrapper = (props) => {
+    const navigate = useNavigate();
+    
+    return (
+      <Component
+        navigate={navigate}
+        {...props}
+        />
+    );
+  };
+  
+  return Wrapper;
+};
