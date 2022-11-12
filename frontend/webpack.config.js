c14dfa3c2e940affe4b76c0e68fb17835e574b16
sven <s@ono.re>
2022-11-12 15:33:55 +0100

    small changes

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -46,14 +46,15 @@ module.exports = {
     new CleanWebpackPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new Dotenv(),
-    /*
+    
     new WorkboxPlugin.GenerateSW({
       // these options encourage the ServiceWorkers to get in there fast
       // and not allow any straggling "old" SWs to hang around
       clientsClaim: true,
       skipWaiting: true,
       maximumFileSizeToCacheInBytes: 5000000,
-    }),*/
+    }),
+    
     
   ],
   module: {
