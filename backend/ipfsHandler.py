5756bfdd48e7b525e3cb52fdca41d337339e8d67
sven <s@ono.re>
2022-12-23 21:02:15 +0100

    add: ipfs cluster support

--- backend/ipfsHandler.py
+++ backend/ipfsHandler.py
@@ -1,6 +1,7 @@
 import traceback
 import requests
 from io import StringIO
+
 import utils
 
 
@@ -8,15 +9,17 @@ class IPFS:
 
     def __init__(self):
         self.ipfsApi = f"http://{utils.IPFS_HOST}:5001/api/v0"
-        
+
         if utils.IPFS_CLUSTER_HOST is not None:
             self.ipfsClusterApi = f"http://{utils.IPFS_CLUSTER_HOST}:9094"
 
-    def storeData(self,data):
+    def storeData(self, data):
+
         # at first get only get hash file because we use this as the name
         with StringIO(data) as f:
             try:
-                response = requests.post(f"{self.ipfsApi}/add", files={"file": f.read()},params={'cid-version':1,"only-hash":1})
+                response = requests.post(f"{self.ipfsApi}/add", files={"file": f.read()},
+                                         params={'cid-version': 1, "only-hash": 1})
 
             except requests.RequestException as e:
                 print(f"Error for getting hash {e}")
@@ -30,29 +33,28 @@ class IPFS:
                 response = requests.post(f"{self.ipfsApi}/add", files={f"{hashFile}": f.read()},
                                          params={'cid-version': 1})
 
-                # will have to add file directly from cluster, but cid-version 1 seems not available
-                if utils.IPFS_CLUSTER_HOST is not None:
-                    try:
-                        response = requests.post(f"{self.ipfsClusterApi}/pins/ipfs/{hashFile}",
-                                                 params={"mode": "recursive"})
-                        if not response.ok:
-                            print(f"Error with cluster pinning, {response.content}")
-
-                    except requests.RequestException as e:
-                        print(f"Error with cluster pinning, {e}")
-
                 if not response.ok:
                     return None
             else:
                 return None
 
+        # will have to add file directly from cluster, but cid-version 1 seems not available
+        
+        if utils.IPFS_CLUSTER_HOST is not None:
+            try:
+                response = requests.post(f"{self.ipfsClusterApi}/pins/ipfs/{hashFile}",
+                                         params={"mode": "recursive"})
+                if not response.ok:
+                    print(f"Error with cluster pinning, {response.content}")
+
+            except requests.RequestException as e:
+                print(f"Error with cluster pinning, {e}")
+
         return hashFile
 
-
-
-    def getData(self,hash):
+    def getData(self, hash):
         try:
-            response = requests.post(f"{self.ipfsApi}/cat",params={'arg':hash})
+            response = requests.post(f"{self.ipfsApi}/cat", params={'arg': hash})
 
             if response.ok:
                 return response.content.decode()
